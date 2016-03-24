a5c78914613a882a4d5016b340bb51a8a00bb512
Camilo S <camiloess@gmail.com>
2016-03-24 13:17:31 -0500

    Role Administrador

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -90,7 +90,7 @@
 	<div class="row">
 		<div id="sidebar-left" class="col-xs-2 col-sm-2">
 			<ul class="nav main-menu">
-				@if (Confide::user()->hasRole('Administradorrrrrr'))
+				@if (Confide::user()->hasRole('Administrador'))
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle">
 							<i class="fa fa-indent"></i>
